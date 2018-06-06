2b5435cdd9b183797b9ab511ca74f38404dcb610
Alexander Soong <alexsoong91@gmail.com>
2018-06-06 11:53:14 -0700

    Enforce contract linting

--- contracts/SetTokenFactory.sol
+++ contracts/SetTokenFactory.sol
@@ -2,8 +2,17 @@ pragma solidity 0.4.23;
 
 import "./SetToken.sol";
 
+
 contract SetTokenFactory {
-  function createSet(address[] _tokens, uint[] _units, uint _naturalUnit) public returns (address) {
-    return new SetToken(_tokens, _units, _naturalUnit);
-  }
+    function createSet(
+        address[] _tokens,
+        uint[] _units,
+        uint _naturalUnit
+    ) 
+        public
+        returns
+        (address) 
+    {
+        return new SetToken(_tokens, _units, _naturalUnit);
+    }
 }

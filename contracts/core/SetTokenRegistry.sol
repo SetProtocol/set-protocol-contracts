67861846cc3e017683d5a5d2e195aab5823e34bb
Alexander Soong <alexsoong91@gmail.com>
2018-06-06 17:59:06 -0700

    Updated compiler versions to 0.4.24

--- contracts/core/SetTokenRegistry.sol
+++ contracts/core/SetTokenRegistry.sol
@@ -1,4 +1,4 @@
-pragma solidity 0.4.23;
+pragma solidity 0.4.24;
 
 import "zeppelin-solidity/contracts/ownership/Ownable.sol";
 import "./interfaces/ISetToken.sol";
--- contracts/test/DummyToken.sol
+++ contracts/test/DummyToken.sol
@@ -1,4 +1,4 @@
-pragma solidity 0.4.23;
+pragma solidity 0.4.24;
 
 import "zeppelin-solidity/contracts/token/ERC20/MintableToken.sol";
 import "zeppelin-solidity/contracts/math/SafeMath.sol";

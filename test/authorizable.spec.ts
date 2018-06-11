93891f2e3dd3359a4b353c47ca03fd203d295a71
bweick <brian.weickmann@gmail.com>
2018-06-11 13:59:21 -0700

    Added events and formatting to Authorizable. Created spec file for authorizable.

---
+++ test/authorizable.spec.ts
@@ -0,0 +1,29 @@
+import * as chai from "chai";
+import * as _ from "lodash";
+
+import * as ABIDecoder from "abi-decoder";
+import { BigNumber } from "bignumber.js";
+
+// Types
+import { Address, Log, UInt } from "../types/common.js";
+
+// Contract types
+import { CoreContract } from "../types/generated/authorizable";
+
+// Artifacts
+const Authorizable = artifacts.require("Authorizable");
+
+// Testing Set up
+import { BigNumberSetup } from "./config/bignumber_setup";
+import ChaiSetup from "./config/chai_setup";
+BigNumberSetup.configure();
+ChaiSetup.configure();
+const { expect, assert } = chai;
+
+import {
+  expectRevertError,
+} from "./utils/tokenAssertions";
+import {
+  STANDARD_INITIAL_TOKENS,
+  UNLIMITED_ALLOWANCE_IN_BASE_UNITS,
+} from "./constants/constants";

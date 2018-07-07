374f0ff80c6da4eb61f174f9429b7a50f8e889fb
Alexander Soong <alexsoong91@gmail.com>
2018-07-07 15:09:40 -0700

    Move erc20 wrapper into general libs

--- contracts/core/TransferProxy.sol
+++ contracts/core/TransferProxy.sol
@@ -19,7 +19,7 @@ pragma solidity 0.4.24;
 
 import { Authorizable } from "../lib/Authorizable.sol";
 import { SafeMath } from "zeppelin-solidity/contracts/math/SafeMath.sol";
-import { ERC20Wrapper } from "./lib/ERC20Wrapper.sol";
+import { ERC20Wrapper } from "../lib/ERC20Wrapper.sol";
 
 
 /**

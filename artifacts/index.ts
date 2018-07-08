8faed5723b3f0a4621ff2f8e4288d016e6dd7473
Justin Chen <justinkchen14@gmail.com>
2018-07-07 18:27:06 -0700

    Remove interfaces from exports

--- artifacts/index.ts
+++ artifacts/index.ts
@@ -7,14 +7,6 @@ import { Core } from "./ts/Core";
 import { DetailedERC20 } from "./ts/DetailedERC20";
 import { ERC20 } from "./ts/ERC20";
 import { ERC20Basic } from "./ts/ERC20Basic";
-import { ICore } from "./ts/ICore";
-import { ICoreIssuance } from "./ts/ICoreIssuance";
-import { IExchange } from "./ts/IExchange";
-import { IERC20 } from "./ts/IERC20";
-import { ISetToken } from "./ts/ISetToken";
-import { ISetFactory } from "./ts/ISetFactory";
-import { ITransferProxy } from "./ts/ITransferProxy";
-import { IVault } from "./ts/IVault";
 import { SetToken } from "./ts/SetToken";
 import { SetTokenFactory } from "./ts/SetTokenFactory";
 import { StandardToken } from "./ts/StandardToken";
@@ -29,14 +21,6 @@ export {
   DetailedERC20,
   ERC20,
   ERC20Basic,
-  IERC20,
-  ICore,
-  ICoreIssuance,
-  IExchange,
-  ISetToken,
-  ISetFactory,
-  ITransferProxy,
-  IVault,
   SetToken,
   SetTokenFactory,
   StandardToken,

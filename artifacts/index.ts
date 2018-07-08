7d58f5488aceaf682c6e0da56ae550f9021895fd
Justin Chen <justinkchen14@gmail.com>
2018-07-07 18:20:13 -0700

    Add back ERC20Basic and StandardToken

--- artifacts/index.ts
+++ artifacts/index.ts
@@ -6,6 +6,7 @@ import { NoXferReturnTokenMock } from "./ts/NoXferReturnTokenMock";
 import { Core } from "./ts/Core";
 import { DetailedERC20 } from "./ts/DetailedERC20";
 import { ERC20 } from "./ts/ERC20";
+import { ERC20Basic } from "./ts/ERC20Basic";
 import { ICore } from "./ts/ICore";
 import { ICoreIssuance } from "./ts/ICoreIssuance";
 import { IExchange } from "./ts/IExchange";
@@ -16,6 +17,7 @@ import { ITransferProxy } from "./ts/ITransferProxy";
 import { IVault } from "./ts/IVault";
 import { SetToken } from "./ts/SetToken";
 import { SetTokenFactory } from "./ts/SetTokenFactory";
+import { StandardToken } from "./ts/StandardToken";
 import { StandardTokenMock } from "./ts/StandardTokenMock";
 import { StandardTokenWithFeeMock } from "./ts/StandardTokenWithFeeMock";
 import { TransferProxy } from "./ts/TransferProxy";
@@ -26,6 +28,7 @@ export {
   Core,
   DetailedERC20,
   ERC20,
+  ERC20Basic,
   IERC20,
   ICore,
   ICoreIssuance,
@@ -36,6 +39,7 @@ export {
   IVault,
   SetToken,
   SetTokenFactory,
+  StandardToken,
   StandardTokenMock,
   StandardTokenWithFeeMock,
   TransferProxy,

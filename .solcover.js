f6584639f94272a560fa0fb67f2e372962ff0b28
Alexander Soong <alexsoong91@gmail.com>
2018-08-06 15:05:24 -0700

    Add coverage

--- .solcover.js
+++ .solcover.js
@@ -1,10 +1,11 @@
 module.exports = {
   port: 8555,
-  testrpcOptions: "--db blockchain --networkId 50 --accounts 20 -p 8555 -e 1000000 -m 'concert load couple harbor equip island argue ramp clarify fence smart topic'",
+  testrpcOptions: "--db blockchain --networkId 50 --port 8555 --accounts 20 -e 1000000 -m 'concert load couple harbor equip island argue ramp clarify fence smart topic'",
   testCommand: 'node --max-old-space-size=4096 ../node_modules/.bin/truffle test --network coverage',
   copyPackages: ['zeppelin-solidity'],
   skipFiles: [
     'Migrations.sol',
-    'mocks'
+    'mocks',
+    'external'
   ],
 };

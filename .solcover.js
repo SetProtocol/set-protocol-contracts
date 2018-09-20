2fd9dede3c0ef3bd7e7654a5a1c4a93af8c415b1
Felix Feng <felix2feng@gmail.com>
2018-09-20 14:38:07 -0700

    Use cryptofin import and remove external folder (#209)
    * Use cryptofin import
    
    * Add additional package to solcover config

--- .solcover.js
+++ .solcover.js
@@ -2,7 +2,7 @@ module.exports = {
   port: 8555,
   testrpcOptions: "--db blockchain --networkId 50 --port 8555 --accounts 20 -e 1000000 -m 'concert load couple harbor equip island argue ramp clarify fence smart topic'",
   testCommand: 'node --max-old-space-size=4096 ../node_modules/.bin/truffle test --network coverage',
-  copyPackages: ['zeppelin-solidity'],
+  copyPackages: ['zeppelin-solidity', 'cryptofin-solidity'],
   skipFiles: [
     'Migrations.sol',
     'mocks',

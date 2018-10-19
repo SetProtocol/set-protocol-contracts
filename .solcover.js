7e4f2a3d81d943b9a2469cfb156d79cbdb196658
Alexander Soong <alexsoong91@gmail.com>
2018-10-19 11:40:59 -0700

    Update path to transpiled tests

--- .solcover.js
+++ .solcover.js
@@ -1,10 +1,9 @@
 module.exports = {
   port: 8555,
   testrpcOptions: "--db blockchain --networkId 50 --port 8555 --accounts 20 -e 1000000 -m 'concert load couple harbor equip island argue ramp clarify fence smart topic'",
-  testCommand: 'node --max-old-space-size=4096 ../node_modules/.bin/truffle test `find transpiled/test/contracts -name '*.spec.js'` --network coverage',
+  testCommand: 'node --max-old-space-size=4096 ../node_modules/.bin/truffle test "find ./transpiled/test/contracts -name '*.spec.js'" --network coverage',
   copyPackages: ['zeppelin-solidity', 'cryptofin-solidity'],
   skipFiles: [
-    'Migrations.sol',
     'mocks',
     'external',
   ],

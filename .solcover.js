ac371be020ac5d542b75931ee3835604185fbd16
Alexander Soong <alexsoong91@gmail.com>
2018-10-22 15:03:41 -0700

    Use find flag

--- .solcover.js
+++ .solcover.js
@@ -1,7 +1,7 @@
 module.exports = {
   port: 8555,
   testrpcOptions: "--db blockchain --networkId 50 --port 8555 --accounts 20 -e 1000000 -m 'concert load couple harbor equip island argue ramp clarify fence smart topic'",
-  testCommand: 'node --max-old-space-size=4096 ../node_modules/.bin/truffle test "find ./transpiled/test/contracts/core/core.spec.ts -name '*.spec.js'" --network coverage',
+  testCommand: 'node --max-old-space-size=4096 ../node_modules/.bin/truffle test `find ./transpiled/test/contracts -name '*.spec.js'` --network coverage',
   copyPackages: ['zeppelin-solidity', 'cryptofin-solidity'],
   skipFiles: [
     'mocks',

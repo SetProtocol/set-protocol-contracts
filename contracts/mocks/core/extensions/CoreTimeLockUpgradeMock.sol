836e109c8a718923d0cf5b1d03c5ee64015a8f05
felix2feng <felix2feng@gmail.com>
2018-11-15 18:15:22 -0800

    Add core internal to mock

--- contracts/mocks/core/extensions/CoreTimeLockUpgradeMock.sol
+++ contracts/mocks/core/extensions/CoreTimeLockUpgradeMock.sol
@@ -2,11 +2,13 @@ pragma solidity 0.4.25;
 pragma experimental "ABIEncoderV2";
 
 import { CoreTimeLockUpgrade } from "../../../core/extensions/CoreTimeLockUpgrade.sol";
+import { CoreInternal } from "../../../core/extensions/CoreInternal.sol";
 import { CoreState } from "../../../core/lib/CoreState.sol";
 
 // Mock contract implementation of CoreTimeLockUpgrade functions
 contract CoreTimeLockUpgradeMock is
     CoreState,
+    CoreInternal,
     CoreTimeLockUpgrade
 {
     uint256 public testUint;

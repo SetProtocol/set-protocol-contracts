8481b7573620f98a998ec00361fdc3d9b5e602bc
felix2feng <felix2feng@gmail.com>
2019-01-18 11:21:09 -0800

    Add back bignumber

--- test/scenarios/rebalancing/btchEthRebalancing.spec.ts
+++ test/scenarios/rebalancing/btchEthRebalancing.spec.ts
@@ -5,6 +5,7 @@ import * as ABIDecoder from 'abi-decoder';
 import * as chai from 'chai';
 import * as setProtocolUtils from 'set-protocol-utils';
 import { SetProtocolTestUtils, Web3Utils } from 'set-protocol-utils';
+import { BigNumber } from 'bignumber.js';
 
 import ChaiSetup from '@utils/chaiSetup';
 import { BigNumberSetup } from '@utils/bigNumberSetup';

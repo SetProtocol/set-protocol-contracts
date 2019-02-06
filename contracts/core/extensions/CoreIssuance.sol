67c42d072e04f9f630d6f183f251dfe7e7a0f56f
felix2feng <felix2feng@gmail.com>
2019-02-05 16:07:24 -0800

    Remove whenOperational when not needed

--- contracts/core/extensions/CoreIssuance.sol
+++ contracts/core/extensions/CoreIssuance.sol
@@ -53,7 +53,6 @@ contract CoreIssuance is
     )
         external
         nonReentrant
-        whenOperational
     {
         issueInternal(
             msg.sender,
@@ -98,7 +97,6 @@ contract CoreIssuance is
     )
         external
         nonReentrant
-        whenOperational
     {
         issueInternal(
             msg.sender,

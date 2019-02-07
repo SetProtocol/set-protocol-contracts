e8153f488fb20156c86e771dc05fa32105864866
felix2feng <felix2feng@gmail.com>
2019-02-07 13:46:26 -0800

    Update deployment script

--- migrations/5_BTCETHRebalancingSetToken.js
+++ migrations/5_BTCETHRebalancingSetToken.js
@@ -19,9 +19,9 @@ const THIRTY_MINUTES_IN_SECONDS = new BigNumber(1800);
 const ONE_HOUR_IN_SECONDS = new BigNumber(3600);
 
 // Unit and naturalUnit constants
-const DEFAULT_REBALANCING_NATURAL_UNIT = new BigNumber(10 ** 10);
+const DEFAULT_REBALANCING_SET_NATURAL_UNIT = new BigNumber(10 ** 6);
 const ETH_DOMINANT_REBALANCING_NATURAL_UNIT = new BigNumber(10 ** 12);
-const DEFAULT_SET_NATURAL_UNIT = new BigNumber(10 ** 10);
+const DEFAULT_BASE_SET_NATURAL_UNIT = new BigNumber(10 ** 10);
 const DEFAULT_WBTC_UNIT = new BigNumber(1);
 const SET_FULL_TOKEN_UNITS = new BigNumber(10 ** 18);
 const WBTC_FULL_TOKEN_UNITS = new BigNumber(10 ** 8);
@@ -34,7 +34,7 @@ const PRICE_PRECISION = new BigNumber(100);
 // Contract Addresses
 const WBTC_MEDIANIZER_ADDRESS_KOVAN = '0x02186378d8e723e11643b4cd520e31655be3b0e9';
 const WBTC_MEDIANIZER_ADDRESS_TESTRPC = '0x2002d3812f58e35f0ea1ffbf80a75a38c32173fa'; // Dummy address
-const WBTC_MEDIANIZER_ADDRESS_MAINNET = '';
+const WBTC_MEDIANIZER_ADDRESS_MAINNET = '0x064409168198A7E9108036D072eF59F923dEDC9A';
 
 const WETH_MEDIANIZER_ADDRESS_KOVAN = '0x9Fe0D478D0E290d50EF8DFc08760C4ad9D2C7AE9';
 const WETH_MEDIANIZER_ADDRESS_TESTRPC = '0x2002d3812f58e35f0ea1ffbf80a75a38c32174fa'; // Dummy address
@@ -165,7 +165,7 @@ async function deployBTCETHRebalancingSet(deployer, network) {
     initialSetParams['units'],
     initialSetParams['naturalUnit'],
   );
-  const rebalancingSetNaturalUnit = DEFAULT_REBALANCING_NATURAL_UNIT;
+  const rebalancingSetNaturalUnit = DEFAULT_REBALANCING_SET_NATURAL_UNIT;
   const rebalancingSetName = SetUtils.stringToBytes(REBALANCING_SET_NAME);
   const rebalancingSetSymbol = SetUtils.stringToBytes(REBALANCING_SET_SYMBOL);
   const rebalancingSetCallData = SetUtils.generateRSetTokenCallData(
@@ -198,7 +198,7 @@ function calculateInitialSetUnits() {
       DEFAULT_WBTC_UNIT.mul(WBTC_MULTIPLIER).toNumber(),
       ethUnits.mul(WETH_MULTIPLIER).toNumber()
     ];
-    naturalUnit = DEFAULT_REBALANCING_NATURAL_UNIT.toNumber();
+    naturalUnit = DEFAULT_BASE_SET_NATURAL_UNIT.toNumber();
   } else {
     const btcUnits = WETH_PRICE.mul(PRICE_PRECISION).div(WBTC_PRICE).round(0, 3);
     const ethUnits = PRICE_PRECISION.mul(DECIMAL_DIFFERENCE_MULTIPLIER);
@@ -234,7 +234,7 @@ function calculateRebalancingSetUnitShares(
   const initialSetDollarAmount = btcDollarAmount.add(ethDollarAmount);
   return [REBALANCING_SET_USD_PRICE
           .div(initialSetDollarAmount)
-          .mul(DEFAULT_REBALANCING_NATURAL_UNIT)
+          .mul(DEFAULT_REBALANCING_SET_NATURAL_UNIT)
           .round(0,3)
           .toNumber()]; 
 }

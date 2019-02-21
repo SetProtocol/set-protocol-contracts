17214b627b2fea19b511969a2d6d85dabd64947a
Felix Feng <felix2feng@gmail.com>
2019-02-21 09:35:02 -0800

    Delete Issuance Orders (#390)
    * Fix issues
    
    * Remove exclude
    
    * Fix coverage

--- test/contracts/lib/commonMath.spec.ts
+++ test/contracts/lib/commonMath.spec.ts
@@ -4,6 +4,9 @@ import * as chai from 'chai';
 import { BigNumber } from 'bignumber.js';
 import { Address } from 'set-protocol-utils';
 
+import { ether } from '@utils/units';
+import { expectRevertError } from '@utils/tokenAssertions';
+
 import ChaiSetup from '@utils/chaiSetup';
 import { BigNumberSetup } from '@utils/bigNumberSetup';
 import { CommonMathMockContract } from '@utils/contracts';
@@ -42,4 +45,59 @@ contract('CommonMathMock', accounts => {
       expect(maxUInt256).to.be.bignumber.equal(expectedMaxUInt256);
     });
   });
+
+  describe('getPartialAmount', async () => {
+    let subjectPrincipal: BigNumber;
+    let subjectNumerator: BigNumber;
+    let subjectDenominator: BigNumber;
+
+    beforeEach(async () => {
+      subjectPrincipal = new BigNumber(1000);
+      subjectNumerator = new BigNumber(400);
+      subjectDenominator = new BigNumber(500);
+    });
+
+    async function subject(): Promise<BigNumber> {
+      return commonMathLibrary.testGetPartialAmount.callAsync(
+        subjectPrincipal,
+        subjectNumerator,
+        subjectDenominator,
+      );
+    }
+
+    it('calculates the partial amount correctly', async () => {
+      const partialAmount = await subject();
+
+      const expectedPartialAmount = subjectPrincipal.mul(subjectNumerator).div(subjectDenominator);
+      expect(partialAmount).to.be.bignumber.equal(expectedPartialAmount);
+    });
+
+    describe('when there is slippage due to rounding', async () => {
+      beforeEach(async () => {
+        subjectPrincipal = ether(10);
+        subjectNumerator = ether(4);
+        subjectDenominator = ether(6);
+      });
+
+      it('should revert', async () => {
+        const partialAmount = await subject();
+
+        const expectedAmount = new BigNumber('6666666666666666666');
+        expect(partialAmount).to.be.bignumber.equal(expectedAmount);
+      });
+
+      describe('when the rounding error is too large', async () => {
+        beforeEach(async () => {
+          subjectPrincipal = new BigNumber(10);
+          subjectNumerator = new BigNumber(4);
+          subjectDenominator = new BigNumber(6);
+        });
+
+        it('should revert', async () => {
+          await expectRevertError(subject());
+        });
+      });
+    });
+  });
+
 });

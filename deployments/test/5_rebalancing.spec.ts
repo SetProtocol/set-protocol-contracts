c2c53da88cecad8b000081c9c025f33c7779ba24
bweick <brian.weickmann@gmail.com>
2019-03-06 10:31:47 -0800

    Small refactor of constants and helpers. Added deploy and tests for btcdai_btd.

--- deployments/test/5_rebalancing.spec.ts
+++ deployments/test/5_rebalancing.spec.ts
@@ -10,9 +10,10 @@ import {
   getNetworkConstant
 } from '../utils/output-helper';
 
-import { calculateInitialSetUnits, calculateETHDaiInitialSetUnits } from '../utils/rebalancing';
+import { calculateInitialSetUnits, calculateGeneralInitialSetUnits } from '../utils/rebalancing';
 import { getWeb3Instance } from '../utils/blockchain';
 
+import { BTCDaiRebalancingManager } from '../../artifacts/ts/BTCDaiRebalancingManager';
 import { BTCETHRebalancingManager } from '../../artifacts/ts/BTCETHRebalancingManager';
 import { ETHDaiRebalancingManager } from '../../artifacts/ts/ETHDaiRebalancingManager';
 import { LinearAuctionPriceCurve } from '../../artifacts/ts/LinearAuctionPriceCurve';
@@ -104,12 +105,12 @@ describe('Deployment: Rebalancing', () => {
 
     it('rebalancing manager has correct wBTC multiplier', async () => {
       const receivedMultiplier = await rebalancingManagerContract.methods.btcMultiplier().call();
-      expect(receivedMultiplier.toString()).toEqual(constants.WBTC.MULTIPLIER.toString());
+      expect(receivedMultiplier.toString()).toEqual(constants.BITETH.WBTC_MULTIPLIER.toString());
     });
 
     it('rebalancing manager has correct wETH multiplier', async () => {
       const receivedMultiplier = await rebalancingManagerContract.methods.ethMultiplier().call();
-      expect(receivedMultiplier.toString()).toEqual(constants.WETH.MULTIPLIER.toString());
+      expect(receivedMultiplier.toString()).toEqual(constants.BITETH.WETH_MULTIPLIER.toString());
     });
 
     it('rebalancing manager has correct lower bound', async () => {
@@ -259,7 +260,7 @@ describe('Deployment: Rebalancing', () => {
   describe('ETHDai Rebalancing Manager', () => {
 
     /**
-     * Check if the BTCETHRebalancingManager was deployed correctly with the following:
+     * Check if the ETHDaiRebalancingManager was deployed correctly with the following:
      * - Core
      * - wethMedianizerAddress
      * - daiAddress
@@ -316,12 +317,12 @@ describe('Deployment: Rebalancing', () => {
 
     it('rebalancing manager has correct Dai multiplier', async () => {
       const receivedMultiplier = await rebalancingManagerContract.methods.daiMultiplier().call();
-      expect(receivedMultiplier.toString()).toEqual(constants.DAI.MULTIPLIER.toString());
+      expect(receivedMultiplier.toString()).toEqual(constants.ETHDAI_BTD.DAI_MULTIPLIER.toString());
     });
 
     it('rebalancing manager has correct wETH multiplier', async () => {
       const receivedMultiplier = await rebalancingManagerContract.methods.ethMultiplier().call();
-      expect(receivedMultiplier.toString()).toEqual(constants.WETH.MULTIPLIER.toString());
+      expect(receivedMultiplier.toString()).toEqual(constants.ETHDAI_BTD.WETH_MULTIPLIER.toString());
     });
 
     it('rebalancing manager has correct lower bound', async () => {
@@ -351,7 +352,15 @@ describe('Deployment: Rebalancing', () => {
      */
 
     let initialCollateralisedSet;
-    const calculatedUnitShares = calculateETHDaiInitialSetUnits();
+    const calculatedUnitShares = calculateGeneralInitialSetUnits(
+      constants.DAI.PRICE,
+      constants.WETH.PRICE,
+      constants.ETHDAI_BTD.DAI_MULTIPLIER,
+      constants.ETHDAI_BTD.WETH_MULTIPLIER,
+      constants.DAI.FULL_TOKEN_UNITS,
+      constants.WETH.FULL_TOKEN_UNITS,
+      constants.ETHDAI_BTD.PRICE_PRECISION,
+    );
 
     before(async () => {
       const initialCollateralSetAddress = await getContractAddress(DEPLOYED_TOKEN.ETHDaiInitialCollateralSet);
@@ -465,6 +474,239 @@ describe('Deployment: Rebalancing', () => {
     it('rebalanced set should have the correct set symbol', async () => {
       const receivedSetSymbol = await ethDaiRebalancingSetToken.methods.symbol().call();
       expect(receivedSetSymbol).toEqual('ETHDai');
+    });
+  });
+
+  describe('BTCDai Rebalancing Manager', () => {
+
+    /**
+     * Check if the BTCDaiRebalancingManager was deployed correctly with the following:
+     * - Core
+     * - wbtcMedianizerAddress
+     * - daiAddress
+     * - wbtcAddress
+     * - SetTokenFactory
+     * - LinearAuctionPriceCurve
+     * - auctionTimeToPivot
+     * - DAI_MULTIPLIER
+     * - WBTC_MULTIPLIER
+     * - lowerBound
+     * - upperBound
+     */
+
+    let rebalancingManagerContract;
+
+    before(async () => {
+      const rebalancingMangerAddress = await getContractAddress(BTCDaiRebalancingManager.contractName);
+      rebalancingManagerContract = new web3.eth.Contract(BTCDaiRebalancingManager.abi, rebalancingMangerAddress);
+    });
+
+    it('find a valid contract at the address', async () => {
+      const code = await getContractCode(BTCDaiRebalancingManager.contractName, web3);
+      expect(code.length).toBeGreaterThan(3);
+    });
+
+    it('rebalancing manager has correct wBTC medianizer address', async () => {
+      const wBTCMedianizerAddress = dependencies.WBTC_MEDIANIZER[networkId];
+      const receivedBTCMedianzierAddress = await rebalancingManagerContract.methods.btcPriceFeed().call();
+      expect(receivedBTCMedianzierAddress).toEqual(wBTCMedianizerAddress);
+    });
+
+    it('rebalancing manager has correct Dai address', async () => {
+      const daiAddress = await findDependency('DAI');
+      const receivedDaiAddress = await rebalancingManagerContract.methods.daiAddress().call();
+      expect(receivedDaiAddress).toEqual(daiAddress);
+    });
+
+    it('rebalancing manger has correct wBTC address', async () => {
+      const wBTCAddress = await findDependency('WBTC');
+      const receivedBTCAddress = await rebalancingManagerContract.methods.btcAddress().call();
+      expect(receivedBTCAddress).toEqual(wBTCAddress);
+    });
+
+    it('rebalancing manager has correct set token factory', async () => {
+      const setTokenFactoryAddress = await getContractAddress(SetTokenFactory.contractName);
+      const receivedSetTokenFactoryAddress = await rebalancingManagerContract.methods.setTokenFactory().call();
+      expect(receivedSetTokenFactoryAddress).toEqual(setTokenFactoryAddress);
+    });
+
+    it('rebalancing manager has correct auction price library instance', async() => {
+      const linearAuctionPriceLibrary = await getContractAddress(LinearAuctionPriceCurve.contractName);
+      const receivedLinearAuctionPriceAddress = await rebalancingManagerContract.methods.auctionLibrary().call();
+      expect(receivedLinearAuctionPriceAddress).toContain(linearAuctionPriceLibrary);
+    });
+
+    it('rebalancing manager has correct auctionTimeToPivot', async () => {
+      const receivedAuctionTimeToPivot = await rebalancingManagerContract.methods.auctionTimeToPivot().call();
+      const auctionTimeToPivot = networkConstants.btcDaiRebalanceManagerAuctionTimeToPivot[networkName].toString();
+      expect(receivedAuctionTimeToPivot.toString()).toEqual(auctionTimeToPivot);
+    });
+
+    it('rebalancing manager has correct Dai multiplier', async () => {
+      const receivedMultiplier = await rebalancingManagerContract.methods.daiMultiplier().call();
+      expect(receivedMultiplier.toString()).toEqual(constants.BTCDAI_BTD.DAI_MULTIPLIER.toString());
+    });
+
+    it('rebalancing manager has correct wBTC multiplier', async () => {
+      const receivedMultiplier = await rebalancingManagerContract.methods.btcMultiplier().call();
+      expect(receivedMultiplier.toString()).toEqual(constants.BTCDAI_BTD.WBTC_MULTIPLIER.toString());
+    });
+
+    it('rebalancing manager has correct lower bound', async () => {
+      const receivevdLowerBound = await rebalancingManagerContract.methods.maximumLowerThreshold().call();
+      const lowerBound = networkConstants.btcDaiRebalanceManagerAllocationLowerBound[networkName].toString();
+      expect(receivevdLowerBound.toString()).toEqual(lowerBound);
+    });
+
+    it('rebalancing manager has correct upper bound', async () => {
+      const receivedUpperBound = await rebalancingManagerContract.methods.minimumUpperThreshold().call();
+      const upperBound = networkConstants.btcDaiRebalanceManagerAllocationUpperBound[networkName].toString();
+      expect(receivedUpperBound.toString()).toEqual(upperBound);
+    });
+  });
+
+  describe('BTCDai Initial Collateralized Set', () => {
+
+    /**
+     * Check if there's a InitialCollateralSet address with the correct params:
+     * - SetTokenFactory
+     * - rebalancingSetComponents
+     * - rebalancingSetUnitShares
+     * - rebalancingSetNaturalUnit
+     * - rebalancingSetName
+     * - rebalancingSetSymbol
+     * - rebalancingSetCallData
+     */
+
+    let initialCollateralisedSet;
+    const calculatedUnitShares = calculateGeneralInitialSetUnits(
+      constants.DAI.PRICE,
+      constants.WBTC.PRICE,
+      constants.BTCDAI_BTD.DAI_MULTIPLIER,
+      constants.BTCDAI_BTD.WBTC_MULTIPLIER,
+      constants.DAI.FULL_TOKEN_UNITS,
+      constants.WBTC.FULL_TOKEN_UNITS,
+      constants.BTCDAI_BTD.PRICE_PRECISION,
+    );
+
+    before(async () => {
+      const initialCollateralSetAddress = await getContractAddress(DEPLOYED_TOKEN.BTCDaiInitialCollateralSet);
+      initialCollateralisedSet = new web3.eth.Contract(SetToken.abi, initialCollateralSetAddress);
+    });
+
+    it('find a valid contract at the address', async () => {
+      const code = await getContractCode(DEPLOYED_TOKEN.BTCDaiInitialCollateralSet, web3);
+      expect(code.length).toBeGreaterThan(3);
+    });
+
+    it('collateralized set should have have the correct set token factory', async () => {
+      const setTokenFactoryAddress = await getContractAddress(SetTokenFactory.contractName);
+      const receivedSetTokenFactoryAddress = await initialCollateralisedSet.methods.factory().call();
+      expect(receivedSetTokenFactoryAddress).toEqual(setTokenFactoryAddress);
+    });
+
+    it('collateralized set should have the correct components', async () => {
+      const wBTCAddress = await findDependency('WBTC');
+      const daiAddress = await findDependency('DAI');
+
+      const setTokenComponents = [daiAddress, wBTCAddress];
+      const receivedSetComponents = await initialCollateralisedSet.methods.getComponents().call();
+      expect(receivedSetComponents).toEqual(setTokenComponents);
+    });
+
+    it('collateralized set should have the correct natural unit', async () => {
+      const receivedNaturalUnit = await initialCollateralisedSet.methods.naturalUnit().call();
+      expect(receivedNaturalUnit.toString()).toEqual(calculatedUnitShares['naturalUnit'].toString());
+    });
+
+    it('collateralized set should have the correct set name', async () => {
+      const receivedSetName = await initialCollateralisedSet.methods.name().call();
+      expect(receivedSetName).toEqual('BTCDAI');
+    });
+
+    it('collateralized set should have the correct set symbol', async () => {
+      const receivedSetSymbol = await initialCollateralisedSet.methods.symbol().call();
+      expect(receivedSetSymbol).toEqual('BTCDAI');
+    });
+  });
+
+  describe('BTCDai Rebalanced Set Token', () => {
+
+    /**
+     * CHeck if there's a rebalanced set with the correct params:
+     * - RebalancingSetTokenFactory address
+     * - rebalancingSetComponents
+     * - rebalancingSetUnitShares
+     * - rebalancingSetNaturalUnit
+     * - rebalancingSetName
+     * - rebalancingSetSymbol
+     * - rebalancingSetCallData
+     */
+
+    let btcDaiRebalancingSetToken;
+
+    before(async () => {
+      const btcDaiRebalancingSetTokenAddress = await getContractAddress(DEPLOYED_TOKEN.BTCDaiRebalancingSetToken);
+      btcDaiRebalancingSetToken = new web3.eth.Contract(RebalancingSetToken.abi, btcDaiRebalancingSetTokenAddress);
+    });
+
+    it('find a valid contract at the address', async () => {
+      const code = await getContractCode('BitEthRebalancingSetToken', web3);
+      expect(code.length).toBeGreaterThan(3);
+    });
+
+    it('rebalanced set should have have the correct rebalancing set token factory', async () => {
+      const rebalancingSetTokenFactoryAddress = await getContractAddress(RebalancingSetTokenFactory.contractName);
+      const receivedSetTokenFactory = await btcDaiRebalancingSetToken.methods.factory().call();
+      expect(receivedSetTokenFactory).toEqual(rebalancingSetTokenFactoryAddress);
+    });
+
+    it('rebalanced set should have the correct components', async () => {
+      const collateralSetAddress = await getContractAddress(DEPLOYED_TOKEN.BTCDaiInitialCollateralSet);
+      const setTokenComponents = [collateralSetAddress];
+      const receivedSetComponents = await btcDaiRebalancingSetToken.methods.getComponents().call();
+      expect(receivedSetComponents).toEqual(setTokenComponents);
+    });
+
+    it('rebalanced set should have the correct natural unit', async () => {
+      const receivedNaturalUnits = await btcDaiRebalancingSetToken.methods.naturalUnit().call();
+      expect(receivedNaturalUnits.toString()).toEqual(constants.DEFAULT_REBALANCING_NATURAL_UNIT.toString());
+    });
+
+    it('rebalanced set should have the correct proposal period', async () => {
+      const receivedProposalPeriod = await btcDaiRebalancingSetToken.methods.proposalPeriod().call();
+      expect(receivedProposalPeriod.toString()).toEqual(networkConstants.btcDaiProposalPeriod[networkName].toString());
+    });
+
+    it('rebalanced set should have the correct rebalance interval', async () => {
+      const receivedRebalanceInterval = await btcDaiRebalancingSetToken.methods.rebalanceInterval().call();
+      expect(
+        receivedRebalanceInterval.toString()
+      ).toEqual(
+        networkConstants.btcDaiRebalanceInterval[networkName].toString()
+      );
+    });
+
+    it('rebalanced set should have the correct manager contract', async () => {
+      const managerAddress = await getContractAddress(BTCDaiRebalancingManager.contractName);
+      const receivedManagerAddress = await btcDaiRebalancingSetToken.methods.manager().call();
+      expect(receivedManagerAddress).toEqual(managerAddress);
+    });
+
+    it('rebalanced set should have the correct whitelist contract', async () => {
+      const whiteListAddress = await getContractAddress(WhiteList.contractName);
+      const receivedWhiteListAddress = await btcDaiRebalancingSetToken.methods.componentWhiteListAddress().call();
+      expect(receivedWhiteListAddress).toEqual(whiteListAddress);
+    });
+
+    it('rebalanced set should have the correct set name', async () => {
+      const receivedSetName = await btcDaiRebalancingSetToken.methods.name().call();
+      expect(receivedSetName).toEqual('BTD BTCDai Set');
+    });
+
+    it('rebalanced set should have the correct set symbol', async () => {
+      const receivedSetSymbol = await btcDaiRebalancingSetToken.methods.symbol().call();
+      expect(receivedSetSymbol).toEqual('BTCDai');
     });
   });
 });
--- deployments/utils/rebalancing.ts
+++ deployments/utils/rebalancing.ts
@@ -6,11 +6,11 @@ export function calculateInitialSetUnits() {
   let naturalUnit: BigNumber = new BigNumber(0);
 
   const WBTC_PRICE = constants.WBTC.PRICE;
-  const WBTC_MULTIPLIER = constants.WBTC.MULTIPLIER;
-  const WETH_MULTIPLIER = constants.WETH.MULTIPLIER;
+  const WBTC_MULTIPLIER = constants.BITETH.WBTC_MULTIPLIER;
+  const WETH_MULTIPLIER = constants.BITETH.WETH_MULTIPLIER;
   const WETH_PRICE = constants.WETH.PRICE;
   const DECIMAL_DIFFERENCE_MULTIPLIER = constants.WETH.FULL_TOKEN_UNITS.div(constants.WBTC.FULL_TOKEN_UNITS);
-  const PRICE_PRECISION = constants.PRICE_PRECISION;
+  const PRICE_PRECISION = constants.BITETH.PRICE_PRECISION;
 
   if (WBTC_PRICE.greaterThanOrEqualTo(WETH_PRICE)) {
     const ethUnits = WBTC_PRICE.mul(DECIMAL_DIFFERENCE_MULTIPLIER).div(WETH_PRICE).round(0, 3);
@@ -18,7 +18,7 @@ export function calculateInitialSetUnits() {
       constants.DEFAULT_WBTC_UNIT.mul(WBTC_MULTIPLIER).toNumber(),
       ethUnits.mul(WETH_MULTIPLIER).toNumber(),
     ];
-    naturalUnit = constants.DEFAULT_REBALANCING_NATURAL_UNIT;
+    naturalUnit = constants.DEFAULT_COLLATERAL_NATURAL_UNIT;
   } else {
     const btcUnits = WETH_PRICE.mul(PRICE_PRECISION).div(WBTC_PRICE).round(0, 3);
     const ethUnits = PRICE_PRECISION.mul(DECIMAL_DIFFERENCE_MULTIPLIER);
@@ -26,7 +26,7 @@ export function calculateInitialSetUnits() {
       btcUnits.mul(WBTC_MULTIPLIER).toNumber(),
       ethUnits.mul(WETH_MULTIPLIER).toNumber(),
     ];
-    naturalUnit = constants.WETH_DOMINANT_REBALANCING_NATURAL_UNIT;
+    naturalUnit = constants.WETH_DOMINANT_COLLATERAL_NATURAL_UNIT;
   }
 
   return {
@@ -53,40 +53,42 @@ export function calculateRebalancingSetUnitShares(
                                 .div(initialSetNaturalUnit)
                                 .round(0, 3);
 
-  const btcDollarAmount = tokenOne.PRICE
+  const tokenOneDollarAmount = tokenOne.PRICE
                             .mul(tokenOneUnitsInFullToken)
                             .div(tokenOne.FULL_TOKEN_UNITS)
                             .round(0, 3);
 
-  const ethDollarAmount = tokenTwo.PRICE
+  const tokenTwoDollarAmount = tokenTwo.PRICE
                             .mul(tokenTwoUnitsInFullToken)
                             .div(tokenTwo.FULL_TOKEN_UNITS)
                             .round(0, 3);
 
-  const initialSetDollarAmount = btcDollarAmount.add(ethDollarAmount);
+  const initialSetDollarAmount = tokenOneDollarAmount.add(tokenTwoDollarAmount);
   return [constants.REBALANCING_SET_USD_PRICE
           .div(initialSetDollarAmount)
           .mul(constants.DEFAULT_REBALANCING_NATURAL_UNIT)
           .round(0, 3)];
 }
 
-export function calculateETHDaiInitialSetUnits(
+export function calculateGeneralInitialSetUnits(
+  tokenOnePrice,
+  tokenTwoPrice,
+  tokenOneMultiplier,
+  tokenTwoMultiplier,
+  tokenOneDecimal,
+  tokenTwoDecimal,
+  pricePrecision,
 ): any {
   let units: BigNumber[];
-  const PRICE_PRECISION = constants.PRICE_PRECISION;
-  const tokenOnePrice = constants.DAI.PRICE;
-  const tokenTwoPrice = constants.WETH.PRICE;
-  const tokenOneMultiplier = constants.DAI.MULTIPLIER;
-  const tokenTwoMultiplier = constants.WETH.MULTIPLIER;
-  const decimalDifference = constants.DAI.FULL_TOKEN_UNITS.div(constants.WETH.FULL_TOKEN_UNITS);
+  const decimalDifference = tokenOneDecimal.div(tokenTwoDecimal);
 
-  const naturalUnit: BigNumber = PRICE_PRECISION.mul(decimalDifference);
+  const naturalUnit: BigNumber = pricePrecision.mul(decimalDifference);
   if (tokenTwoPrice.greaterThanOrEqualTo(tokenOnePrice)) {
-    const tokenOneUnits = tokenTwoPrice.mul(decimalDifference).mul(PRICE_PRECISION).div(tokenOnePrice).round(0, 3);
-    units = [tokenOneMultiplier.mul(tokenOneUnits), tokenTwoMultiplier.mul(PRICE_PRECISION)];
+    const tokenOneUnits = tokenTwoPrice.mul(decimalDifference).mul(pricePrecision).div(tokenOnePrice).round(0, 3);
+    units = [tokenOneMultiplier.mul(tokenOneUnits), tokenTwoMultiplier.mul(pricePrecision)];
   } else {
-    const tokenTwoUnits = tokenOnePrice.mul(PRICE_PRECISION).div(tokenTwoPrice).round(0, 3);
-    units = [PRICE_PRECISION.mul(decimalDifference).mul(tokenOneMultiplier), tokenTwoUnits];
+    const tokenTwoUnits = tokenOnePrice.mul(pricePrecision).div(tokenTwoPrice).round(0, 3);
+    units = [pricePrecision.mul(decimalDifference).mul(tokenOneMultiplier), tokenTwoUnits.mul(tokenTwoMultiplier)];
   }
 
   return {

20a7b395fccee1f628582ae3959a8404349f721f
felix2feng <felix2feng@gmail.com>
2019-03-12 14:45:10 -0700

    Remove migration.sol logic

--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -1,24 +1,3 @@
 pragma solidity 0.5.4;
 
-
-contract Migrations {
-  address public owner;
-  uint256 public lastCompletedMigration;
-
-  modifier restricted() {
-    if (msg.sender == owner) _;
-  }
-
-  constructor() public {
-    owner = msg.sender;
-  }
-
-  function setCompleted(uint256 completed) external restricted {
-    lastCompletedMigration = completed;
-  }
-
-  function upgrade(address newAddress) external restricted {
-    Migrations upgraded = Migrations(newAddress);
-    upgraded.setCompleted(lastCompletedMigration);
-  }
-}
+contract Migrations {}

342c5ed4badcf8107f6f98b25f9f8bf7a544de58
Richard Liang <richardliang2015@u.northwestern.edu>
2019-10-28 16:20:24 -0700

    Artifacts export fix

--- artifacts/index.ts
+++ artifacts/index.ts
@@ -68,7 +68,7 @@ export {
   ProposeLibraryContract,
   ProtocolViewerContract,
   RebalanceAuctionModuleContract,
-  RebalancingSetEthBidder,
+  RebalancingSetEthBidderContract,
   RebalancingSetExchangeIssuanceModuleContract,
   RebalancingSetIssuanceModuleContract,
   RebalancingSetTokenContract,

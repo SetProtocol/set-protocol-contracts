318ddf613bb2055cd62b7c79fee47b49a3bff69f
felix2feng <felix2feng@gmail.com>
2019-11-06 20:46:39 -0800

    Fix script

--- scripts/fix-web3-eth-abi.sh
+++ scripts/fix-web3-eth-abi.sh
@@ -1,8 +1,8 @@
 if [[ "$OSTYPE" == "darwin"* ]]; then
   # Mac OSX
-  [ -f "node_modules/web3-eth-abi/src/index.js" ] && sed -i '' 's/(nonIndexedData)/(nonIndexedData \&\& nonIndexedData !== "0x" \&\& nonIndexedData !== "")/' node_modules/web3-eth-abi/src/index.js
-  [ -f "node_modules/web3-eth-contract/src/index.js" ] && sed -i '' 's/abi.decodeParameters(outputs, returnValues)/(outputs \&\& outputs !== "0x" \&\& outputs !== "") ? abi.decodeParameters(outputs, returnValues) : []/' node_modules/web3-eth-contract/src/index.js
+  [ -f "node_modules/web3-eth-abi/src/index.js" ] && sed -i '' 's/(nonIndexedData)/(nonIndexedData \&\& nonIndexedData !== "0x")/' node_modules/web3-eth-abi/src/index.js
+  [ -f "node_modules/web3-eth-contract/src/index.js" ] && sed -i '' 's/abi.decodeParameters(outputs, returnValues)/(returnValues \&\& returnValues !== "0x") ? abi.decodeParameters(outputs, returnValues) : []/' node_modules/web3-eth-contract/src/index.js
 else
-  [ -f "node_modules/web3-eth-abi/src/index.js" ] && sed -i 's/(nonIndexedData)/(nonIndexedData \&\& nonIndexedData !== "0x" \&\& nonIndexedData !== "")/' node_modules/web3-eth-abi/src/index.js
-  [ -f "node_modules/web3-eth-contract/src/index.js" ] && sed -i 's/abi.decodeParameters(outputs, returnValues)/(outputs \&\& outputs !== "0x" \&\& outputs !== "") ? abi.decodeParameters(outputs, returnValues) : []/' node_modules/web3-eth-contract/src/index.js
+  [ -f "node_modules/web3-eth-abi/src/index.js" ] && sed -i 's/(nonIndexedData)/(nonIndexedData \&\& nonIndexedData !== "0x")/' node_modules/web3-eth-abi/src/index.js
+  [ -f "node_modules/web3-eth-contract/src/index.js" ] && sed -i 's/abi.decodeParameters(outputs, returnValues)/(returnValues \&\& returnValues !== "0x") ? abi.decodeParameters(outputs, returnValues) : []/' node_modules/web3-eth-contract/src/index.js
 fi

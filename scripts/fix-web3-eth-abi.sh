cc2d8e31d5679f6c75302f9bc2fa05a96056ba88
bweick <brian.weickmann@gmail.com>
2019-12-06 14:48:44 -0800

    Fix script bug.

--- scripts/fix-web3-eth-abi.sh
+++ scripts/fix-web3-eth-abi.sh
@@ -6,3 +6,5 @@ else
   [ -f "node_modules/web3-eth-abi/src/index.js" ] && sed -i 's/(nonIndexedData)/(nonIndexedData \&\& nonIndexedData !== "0x")/' node_modules/web3-eth-abi/src/index.js
   [ -f "node_modules/web3-eth-contract/src/index.js" ] && sed -i 's/abi.decodeParameters(outputs, returnValues)/(returnValues \&\& returnValues !== "0x") ? abi.decodeParameters(outputs, returnValues) : []/' node_modules/web3-eth-contract/src/index.js
 fi
+
+echo "fix-web3-eth-abi script finished!"

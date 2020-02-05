d1a1da510e6d48339b48d43f84477a08f2b720c9
Felix Feng <felix2feng@gmail.com>
2020-02-05 00:00:13 -0800

    Export FactoryUtilsLIbraryContract (#576)
    * Export
    
    * Bump to 22

--- utils/contracts.ts
+++ utils/contracts.ts
@@ -22,6 +22,7 @@ export { ExchangeIssuanceLibraryContract } from '../types/generated/exchange_iss
 export { ExchangeIssuanceLibraryMockContract } from '../types/generated/exchange_issuance_library_mock';
 export { ExchangeIssuanceModuleContract } from '../types/generated/exchange_issuance_module';
 export { FailAuctionLibraryContract } from '../types/generated/fail_auction_library';
+export { FactoryUtilsLibraryContract } from '../types/generated/factory_utils_library';
 export { FeeCalculatorMockContract } from '../types/generated/fee_calculator_mock';
 export { FixedFeeCalculatorMockContract } from '../types/generated/fixed_fee_calculator_mock';
 export { FeedFactoryContract } from '../types/generated/feed_factory';

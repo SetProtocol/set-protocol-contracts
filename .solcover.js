e51c8c507f93b9da8024f6c7e8d3ce16b8b8196d
bweick <brian.weickmann@gmail.com>
2020-04-21 12:41:04 -0700

    Added set-protocol-contract-utils to copyPackage.

--- .solcover.js
+++ .solcover.js
@@ -2,7 +2,10 @@ module.exports = {
   port: 8555,
   testrpcOptions: "--db blockchain --networkId 50 --port 8555 --accounts 20 -e 1000000 -m 'concert load couple harbor equip island argue ramp clarify fence smart topic'",
   testCommand: "node --max-old-space-size=4096 ../node_modules/.bin/truffle test `find ./transpiled/test/contracts -name '*.spec.js'` --network coverage",
-  copyPackages: ['openzeppelin-solidity'],
+  copyPackages: [
+    'openzeppelin-solidity',
+    'set-protocol-contract-utils'
+  ],
   skipFiles: [
     'Migrations.sol',
     'lib/AddressArrayUtils.sol',

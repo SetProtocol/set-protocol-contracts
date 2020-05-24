11d54e38a704fe8ee364ec5c6419622c118267d5
Felix Feng <felix2feng@gmail.com>
2020-05-23 21:43:03 -0700

    Add Buidler Native Solc Support (#642)
    * Test
    
    * Delete package lock
    
    * Add useful message

--- buidler.config.ts
+++ buidler.config.ts
@@ -1,26 +1,51 @@
-import { BuidlerConfig, usePlugin } from "@nomiclabs/buidler/config";
+import { BuidlerConfig, usePlugin, internalTask } from "@nomiclabs/buidler/config";
+import { TASK_COMPILE_RUN_COMPILER } from "@nomiclabs/buidler/builtin-tasks/task-names";
+import { execSync } from "child_process";
 
 usePlugin("@nomiclabs/buidler-truffle5");
 usePlugin("@nomiclabs/buidler-web3");
 
+internalTask(TASK_COMPILE_RUN_COMPILER).setAction(async ({ input }, { config }, runSuper) => {
+  let solcVersionOutput = "";
+  try {
+    solcVersionOutput = execSync(`solc --version`).toString();
+  } catch (error) {
+    // Probably failed because solc wasn't installed. We do nothing here.
+  }
+
+  console.log("Output", solcVersionOutput);
+
+  if (!solcVersionOutput.includes(config.solc.version)) {
+    console.log(`Using solcjs`);
+    return runSuper();
+  }
+
+  console.log(`Using native solc`);
+  const output = execSync(`solc --standard-json`, {
+    input: JSON.stringify(input, undefined, 2),
+  });
+
+  return JSON.parse(output.toString(`utf8`));
+});
+
 const config: BuidlerConfig = {
   solc: {
     version: "0.5.7",
-	optimizer: {
-	  enabled: true,
-	  runs: 200
-	},
-    evmVersion: "byzantium"
+    optimizer: {
+      enabled: true,
+      runs: 200,
+    },
+    evmVersion: "byzantium",
   },
   paths: {
-  	artifacts: './build/contracts',
-  	tests: './transpiled/test/contracts'
+    artifacts: "./build/contracts",
+    tests: "./transpiled/test/contracts",
   },
   networks: {
-	  buidlerevm: {
-	  	blockGasLimit: 20000000
-	  }
-  }
+    buidlerevm: {
+      blockGasLimit: 20000000,
+    },
+  },
 };
 
-export default config; 
+export default config;
